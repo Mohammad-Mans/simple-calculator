ea3e0f4458f4c0c3d8c372320b0754b41dff2bdd
Mohammad Mansour <mohammed.i02@hotmail.com>
2023-10-07 04:37:59 +0300

    Step 2: pass provided test cases

--- calculator.js
+++ calculator.js
@@ -1,26 +1,98 @@
+function isValidNumber(num) {
+  if (typeof num !== "number") {
+    throw new Error("Invalid input type");
+  }
+}
+
 function calc(...args) {
-    if (args.length !== 3) {
-      throw new Error('Invalid input');
-    }
-  
-    const [num1, operator, num2] = args;
-  
-    switch (operator) {
-      case '+':
-        return num1 + num2;
-      case '-':
-        return num1 - num2;
-      case '*':
-        return num1 * num2;
-      case '/':
-        if (num2 === 0) {
-          throw new Error('Division by zero');
+  if (args.length < 3 || args.length % 2 == 0) {
+    throw new Error("Invalid input");
+  }
+
+  isValidNumber(args[0]);
+
+  let sum = args[0],
+    lastOperator = "*",
+    lastOperand = args[0],
+    currentOperator,
+    currentOperand,
+    reverted;
+
+  for (let i = 1; i < args.length; ) {
+    reverted = false;
+
+    currentOperator = args[i++];
+    currentOperand = args[i++];
+
+    isValidNumber(currentOperand);
+
+    switch (currentOperator) {
+      case "+":
+        sum += currentOperand;
+        break;
+
+      case "-":
+        sum -= currentOperand;
+        break;
+
+      case "*":
+        
+        if (lastOperator === "+") {
+            
+            sum -= lastOperand;
+            lastOperand = lastOperand * currentOperand;
+            sum += lastOperand;
+            reverted = true;
+
+        } else if(lastOperator === "-") {
+            
+            sum += lastOperand;
+            lastOperand = lastOperand * currentOperand;
+            sum -= lastOperand;
+            reverted = true;
+
         }
-        return num1 / num2;
+         else {
+          sum *= currentOperand;
+        }
+        break;
+
+      case "/":
+        if (currentOperand === 0) {
+          throw new Error("Division by zero");
+        }
+
+        if (lastOperator === "+") {
+            
+            sum -= lastOperand;
+            lastOperand = lastOperand / currentOperand;
+            sum += lastOperand;
+            reverted = true;
+
+        } else if(lastOperator === "-") {
+            
+            sum += lastOperand;
+            lastOperand = lastOperand / currentOperand;
+            sum -= lastOperand;
+            reverted = true;
+            
+        }
+         else {
+          sum /= currentOperand;
+        }
+        break;
+
       default:
-        throw new Error('Invalid operator');
+        throw new Error("Invalid operator");
+    }
+
+    if (!reverted) {
+      lastOperator = currentOperator;
+      lastOperand = currentOperand;
     }
   }
 
+  return sum;
+}
+
 module.exports = calc;
-  
